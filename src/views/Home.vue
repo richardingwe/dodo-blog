86dd9ee3c24942271877eac460daa9c896e0bc2b
richardingwe <richardingwe5@gmail.com>
2021-07-21 08:42:07 +0100

    created singlePost and Post list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
 	<div class="home">
-		home
+		<h1>Home</h1>
+		<PostList :posts="posts" />
 	</div>
 </template>
 
 <script>
-	// @ is an alias to /src
-
+	import { ref } from 'vue';
+	// component imports
+	import PostList from '../components/PostList.vue';
 	export default {
 		name: 'Home',
-		components: {},
+		components: { PostList },
+		setup() {
+			const posts = ref([
+				{
+					title: 'welcome to the blog',
+					body:
+						'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce in imperdiet nisi. Vestibulum eu dui dictum, ornare est non, dignissim dui. Donec at ex nulla. Aenean sem tortor, bibendum ac accumsan vel, elementum vel turpis. Nam auctor egestas pulvinar. Aenean placerat finibus finibus. Praesent dictum velit at purus aliquam, sit amet mattis est molestie. Duis a nulla et mauris luctus feugiat nec cursus odio. Nam elementum vitae est eu porttitor. Nulla vestibulum gravida magna eu ultricies. Nunc posuere tincidunt pellentesque. Sed id condimentum nisl, ultricies viverra orci. Fusce sit amet massa ut nibh feugiat elementum ac id est.',
+					id: 1,
+				},
+				{ title: 'top 5 CSS tips', body: 'lorem ipsum', id: 2 },
+			]);
+
+			return { posts };
+		},
 	};
 </script>

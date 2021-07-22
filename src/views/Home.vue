f15109a40b86fea16b2dc741f87c33f557f963ac
richardingwe <richardingwe5@gmail.com>
2021-07-22 05:02:45 +0100

    added styles to tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="home">
 		<div v-if="error">{{ error }}</div>
-		<div v-if="posts.length">
+		<div v-if="posts.length" class="layout">
 			<PostList :posts="posts" />
 			<TagCloud :posts="posts" />
 		</div>
@@ -12,11 +12,12 @@
 </template>
 
 <script>
+	import { ref } from 'vue';
 	import getPosts from '../composables/getPosts';
+	// component imports
 	import PostList from '../components/PostList.vue';
 	import Spinner from '../components/Spinner.vue';
 	import TagCloud from '../components/TagCloud.vue';
-
 	export default {
 		name: 'Home',
 		components: { PostList, Spinner, TagCloud },
@@ -34,5 +35,10 @@
 		max-width: 1200px;
 		margin: 0 auto;
 		padding: 10px;
+	}
+	.layout {
+		display: grid;
+		grid-template-columns: 3fr 1fr;
+		gap: 20px;
 	}
 </style>
--- src/views/Tag.vue
+++ src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
 	<div class="tag">
 		<div v-if="error">{{ error }}</div>
-		<div v-if="posts.length">
+		<div v-if="posts.length" class="layout">
 			<PostList :posts="postsWithTag" />
+			<TagCloud :posts="posts" />
 		</div>
 		<div v-else>
 			<Spinner />
@@ -11,13 +12,14 @@
 </template>
 
 <script>
+	import TagCloud from '../components/TagCloud.vue';
 	import Spinner from '../components/Spinner.vue';
 	import PostList from '../components/PostList.vue';
 	import getPosts from '../composables/getPosts';
 	import { useRoute } from 'vue-router';
 	import { computed } from 'vue';
 	export default {
-		components: { PostList, Spinner },
+		components: { PostList, Spinner, TagCloud },
 		setup() {
 			const route = useRoute();
 			const { posts, error, load } = getPosts();
